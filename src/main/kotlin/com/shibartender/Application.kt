f690271f51ab4866a5a1efeaeaf6ad19efb3903b
core <cornet.remi@gmail.com>
2023-01-25 00:08:35 -0500

    Add User model and routing + authentication

--- src/main/kotlin/com/shibartender/Application.kt
+++ src/main/kotlin/com/shibartender/Application.kt
@@ -4,16 +4,15 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.shibartender.error.ErrorResponse
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.shibartender.plugins.*
 import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.netty.*
 import io.ktor.server.response.*
 
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     val secret = environment.config.property("jwt.secret").getString()
@@ -45,36 +44,3 @@ fun Application.module() {
     configureRouting()
     configureSerialization()
 }
-//fun main() {
-//    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-//        .start(wait = true)
-//}
-
-//fun Application.module() {
-//    val secret = environment.config.property("jwt.secret").getString()
-//    val issuer = environment.config.property("jwt.issuer").getString()
-//    val audience = environment.config.property("jwt.audience").getString()
-//    val myRealm = environment.config.property("jwt.realm").getString()
-//    install(Authentication) {
-//        jwt("auth-jwt") {
-//            realm = myRealm
-//            verifier(JWT
-//                .require(Algorithm.HMAC256(secret))
-//                .withAudience(audience)
-//                .withIssuer(issuer)
-//                .build())
-//            validate { credential ->
-//                if (credential.payload.getClaim("username").asString() != "") {
-//                    JWTPrincipal(credential.payload)
-//                } else {
-//                    null
-//                }
-//            }
-//            challenge { defaultScheme, realm ->
-//                call.respond(HttpStatusCode.Unauthorized, ErrorResponse.INVALID_TOKEN)
-//            }
-//        }
-//    }
-//    configureSerialization()
-//    configureRouting()
-//}
