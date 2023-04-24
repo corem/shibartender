65b87a4399fac8199345c7a05e15f53de6eb7075
core <cornet.remi@gmail.com>
2023-04-23 20:00:13 -0400

    Env

--- src/main/kotlin/com/shibartender/Application.kt
+++ src/main/kotlin/com/shibartender/Application.kt
@@ -10,6 +10,12 @@ import io.ktor.server.netty.*
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
+    println(environment.config.propertyOrNull("ktor.deployment.port")?.getString() ?: "Not found")
+    println(environment.config.propertyOrNull("ktor.jwt.secret")?.getString() ?: "Not found")
+    println(environment.config.propertyOrNull("ktor.jwt.issuer")?.getString() ?: "Not found")
+    println(environment.config.propertyOrNull("ktor.jwt.validity_ms")?.getString() ?: "Not found")
+    println(environment.config.propertyOrNull("ktor.mongo.uri")?.getString() ?: "Not found")
+
     configureKoin()
     configureAuthentication()
     configureRouting()
