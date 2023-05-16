5a756f737b0a0c64c5865d197cbd5b9518bcb89c
core <cornet.remi@gmail.com>
2023-05-15 22:19:06 -0400

    Env

--- src/main/kotlin/com/shibartender/plugins/Routing.kt
+++ src/main/kotlin/com/shibartender/plugins/Routing.kt
@@ -6,12 +6,9 @@ import com.shibartender.routes.randomRouting
 import com.shibartender.routes.userRouting
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        staticFiles("/favicon", File("favicon.ico"))
         accountRouting()
         userRouting()
         cocktailRouting()
